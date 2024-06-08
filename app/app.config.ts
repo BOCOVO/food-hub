5c66d0086ec736a3b918608399cfb15c03065e6e
bocovo <bocovojuste@gmail.com>
2024-06-08 12:27:27 +0100

    chore(app): install storybook

---
+++ app/app.config.ts
@@ -0,0 +1,9 @@
+export default ({ config }: { config: Record<string, object> }) => {
+  return {
+    ...config,
+    extra: {
+      ...config.extra,
+      storybookEnabled: process.env.STORYBOOK_ENABLED,
+    },
+  };
+};
--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -6,7 +6,8 @@ import {
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { PropsWithChildren, useEffect } from "react";
+import Constants from "expo-constants";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
@@ -14,7 +15,7 @@ import { useColorScheme } from "@/hooks/useColorScheme";
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+function Wrapper({ children }: PropsWithChildren) {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
@@ -32,7 +33,31 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack />
+      {children}
     </ThemeProvider>
   );
 }
+
+const AppContent = () => {
+  return <Stack />;
+};
+
+// Default to rendering your app
+let AppEntryPoint = () => (
+  <Wrapper>
+    <AppContent />
+  </Wrapper>
+);
+
+// Render Storybook if storybookEnabled is true
+if (Constants.expoConfig?.extra?.storybookEnabled === "true") {
+  const Storybook = require("../.storybook").default;
+  const StoryApp = () => (
+    <Wrapper>
+      <Storybook />
+    </Wrapper>
+  );
+  AppEntryPoint = StoryApp;
+}
+
+export default AppEntryPoint;
