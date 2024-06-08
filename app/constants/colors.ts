165c5c522c043ee487590fd570ba5481268992b7
bocovo <bocovojuste@gmail.com>
2024-06-08 15:59:47 +0100

    refactor(app): create constant for colors

---
+++ app/constants/colors.ts
@@ -0,0 +1,29 @@
+// `An` in colors name means anonymous color, colors that used in the Figma
+// but not defined in the design system
+
+export const Colors = {
+  primary: "#FE724C",
+  primary80: "#FE8160",
+  primary50: "#FEA58D",
+  primary20: "#FED2C7",
+
+  secondary: "#FFC529",
+  secondary80: "#FFD050",
+  secondary50: "#FFDF8B",
+  secondary20: "#FFEFC3",
+
+  dark: "#1A1D26",
+  dark80: "#2A2F3D",
+  dark50: "#4D5364",
+  dark20: "#6E7489",
+
+  gray: "#9A9FAE",
+  gray80: "#A8ACB9",
+  gray50: "#C4C7D0",
+  gray20: "#EBEBEB",
+
+  grayAn1: "#C4C4C4",
+  grayAn2: "#D0D2D1",
+  inputShadow: "#D3D1D8",
+  inputShadowWithAlpha: "#d3d1d8c0",
+};
--- app/theme/gluestack-ui.config.ts
+++ app/theme/gluestack-ui.config.ts
@@ -1,33 +1,10 @@
+import { Colors } from "@/constants/colors";
 import { config } from "@gluestack-ui/config";
 import merge from "lodash/merge";
 
 const custom = {
   tokens: {
-    colors: {
-      primary: "#FE724C",
-      primary80: "#FE8160",
-      primary50: "#FEA58D",
-      primary20: "#FED2C7",
-
-      secondary: "#FFC529",
-      secondary80: "#FFD050",
-      secondary50: "#FFDF8B",
-      secondary20: "#FFEFC3",
-
-      dark: "#1A1D26",
-      dark80: "#2A2F3D",
-      dark50: "#4D5364",
-      dark20: "#6E7489",
-
-      gray: "#9A9FAE",
-      gray80: "#A8ACB9",
-      gray50: "#C4C7D0",
-      gray20: "#EBEBEB",
-      // An means anonymous color, colors that used in the Figma
-      // but not defined in the design system
-      grayAn1: "#C4C4C4",
-      grayAn2: "#D0D2D1",
-    },
+    colors: Colors,
     fonts: {
       heading: "SofiaPro",
       body: "SofiaPro",
