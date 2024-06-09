2ef731a4e9fc7031b937f150e16b9cb9a8c98467
bocovo <bocovojuste@gmail.com>
2024-06-09 15:04:43 +0100

    chore(ci): remove icon from primary button storybook

--- app/components/buttons/PrimarySolidButton.stories.tsx
+++ app/components/buttons/PrimarySolidButton.stories.tsx
@@ -1,13 +1,11 @@
 import { Meta, StoryObj } from "@storybook/react";
 import PrimarySolidButton from "./PrimarySolidButton";
-import { AlertCircleIcon } from "@gluestack-ui/themed";
 
 export default {
   title: "components/buttons/PrimarySolidButton",
   component: PrimarySolidButton,
   args: {
     label: "Primary Solid Button",
-    icon: <AlertCircleIcon size="lg" />,
   },
 } as Meta<typeof PrimarySolidButton>;
 
