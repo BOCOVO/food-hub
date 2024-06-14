2c813dfa5968ec3475ebb4e3436889d39d423f1b
bocovo <bocovojuste@gmail.com>
2024-06-14 23:32:30 +0100

    chore(app): little improvement on home page

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,9 +1,20 @@
-import {Text} from "react-native"
+import { Text } from "@/components/core/Text";
+import { Box } from "@gluestack-ui/themed";
+import { Link } from "expo-router";
 
 const Page = () => {
   return (
-    <Text>Home</Text>
-  )
-}
+    <Box pl={20} pt={200}>
+      <Text size="3xl">Welcome to</Text>
+      <Text mb={50} size="3xl" color="$primary">
+        Food-hub
+      </Text>
 
-export default Page
+      <Link href="register">
+        <Text>Go to register</Text>
+      </Link>
+    </Box>
+  );
+};
+
+export default Page;
